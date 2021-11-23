e1d30dffe7be2fd1da7c8c3a9e18d40fc4c6c2c3
JosephNaa <js.pekah@gmail.com>
2021-11-23 23:48:44 +0900

    3. Understanding Unidirectional Data Flow

--- week 2-2-Using state in Jetpack Compose/StateCodelab/app/src/main/java/js/pekah/statecodelab/MainActivity.kt
+++ week 2-2-Using state in Jetpack Compose/StateCodelab/app/src/main/java/js/pekah/statecodelab/MainActivity.kt
@@ -2,10 +2,22 @@ package js.pekah.statecodelab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import js.pekah.statecodelab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var name = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /* ... */
+        binding.textInput.doAfterTextChanged {text ->
+            name = text.toString()
+            updateHello()
+        }
+    }
+
+    private fun updateHello() {
+        binding.helloText.text = "Hello, $name"
     }
 }
