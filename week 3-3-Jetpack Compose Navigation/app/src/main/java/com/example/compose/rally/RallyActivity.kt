9e6bbf2786051b17c86d29024ea26faab4172c88
JosephNaa <js.pekah@gmail.com>
2021-12-04 19:39:31 +0900

    4. Navigating with arguments

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -17,7 +17,6 @@
 package com.example.compose.rally
 
 import android.os.Bundle
-import android.service.autofill.UserData
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -30,12 +29,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.compose.rally.data.UserData
 import com.example.compose.rally.data.UserData.accounts
+import com.example.compose.rally.data.UserData.getAccount
 import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.accounts.SingleAccountBody
 import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.overview.OverviewBody
@@ -73,6 +78,8 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
+            val accountsName = RallyScreen.Accounts.name
+
             NavHost(
                 navController = navController,
                 startDestination = RallyScreen.Overview.name,
@@ -82,10 +89,23 @@ fun RallyApp() {
                     OverviewBody(
                         onClickSeeAllAccounts = {navController.navigate(RallyScreen.Accounts.name)},
                         onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) },
+                        onAccountClick = {name ->
+                            navigateToSingleAccount(navController, name)
+                        }
                     )
                 }
-                composable(RallyScreen.Accounts.name) {
-                    AccountsBody(accounts = com.example.compose.rally.data.UserData.accounts)
+
+                composable(
+                    route = "$accountsName/{name}",
+                    arguments = listOf(
+                        navArgument("name") {
+                            type = NavType.StringType
+                        }
+                    )
+                ) { entry -> // NavBackStackEntry의 인자들중 "name"을 검색한다.
+                    val accountName = entry.arguments?.getString("name")
+                    val account = UserData.getAccount(accountName)
+                    SingleAccountBody(account = account)
                 }
                 composable(RallyScreen.Bills.name) {
                     BillsBody(bills = com.example.compose.rally.data.UserData.bills)
@@ -93,4 +113,11 @@ fun RallyApp() {
             }
         }
     }
+}
+
+private fun navigateToSingleAccount(
+    navController: NavHostController,
+    accountName: String
+) {
+    navController.navigate("${RallyScreen.Accounts.name}/$accountName")
 }
