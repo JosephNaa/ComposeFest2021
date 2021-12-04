91d243d6d63784c2fbfef14884df67e2207338f0
JosephNaa <js.pekah@gmail.com>
2021-12-04 20:30:13 +0900

    11. Interop theming

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -48,6 +49,14 @@ fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
 
     plant?.let {
         PlantDetailContent(it)
+    }
+}
+
+@Preview
+@Composable
+private fun PlantNamePreview() {
+    MdcTheme {
+        PlantName("Apple")
     }
 }
 
